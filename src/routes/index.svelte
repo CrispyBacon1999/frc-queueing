7b6f407f6886e0ef505af7cd3b4f434c00c360da
Josh Bacon <bacon.josh09@gmail.com>
2022-05-22 01:26:04 -0400

    Add home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,49 @@
-<script>
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
-	import QueueingTable from '../components/QueueingTable.svelte';
+<script lang="ts">
+	let eventCode: string = '';
 </script>
 
-<QueueingTable eventCode="2022mimid" />
+<div>
+	<h1>FRC Queueing</h1>
+
+	<p>
+		Provides a queuing display for the pits that automatically scrolls to the latest match without
+		scores.
+	</p>
+
+	<input placeholder="TBA Event Code" bind:value={eventCode} />
+	<button
+		disabled={eventCode.match(/\d{4}[\w\d]{3,8}/) === null}
+		on:click={() => (window.location.href = `/${eventCode}`)}>Go to event</button
+	>
+</div>
+
+<style>
+	* {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+		text-align: center;
+	}
+
+	input {
+		font-size: 18px;
+		padding: 0.5em;
+		border-radius: 8px;
+	}
+
+	button {
+		font-size: 18px;
+		padding: 0.75em;
+		border-radius: 0;
+		border: none;
+		background-color: aqua;
+	}
+
+	button:hover {
+		background-color: aquamarine;
+		cursor: pointer;
+	}
+
+	button:disabled {
+		background-color: lightblue;
+	}
+</style>
